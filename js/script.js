24841ca55c1c683457414f8fcc3e8d77f0745ec8
Kajan <sabakajan1292001@gmail.com>
2022-05-20 14:43:19 +0530

    commit 4

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
 let navbar = document.querySelector('.navbar');
-
-
-
-
-
-window.onscroll = () =>{
-    navbar.classList.remove('active');
-    searchForm.classList.remove('active');
-    cartItem.classList.remove('active');
-}
